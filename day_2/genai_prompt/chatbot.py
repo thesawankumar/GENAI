ace41016be34514e5d18b2a34b84b22612c689ad
thesawankumar <sawankushwaha222@gmail.com>
2026-01-24 21:06:06 +0530

    SyatemMessage,HumanMessage,AIMessage,MessagePlaceHolder Chat History

--- day_2/genai_prompt/chatbot.py
+++ day_2/genai_prompt/chatbot.py
@@ -5,3 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 model = ChatOpenAI()
+
+chat_history = [ SystemMessage(content='You are a helpful AI assistant')]
+
+while True:
+    user_input = input('You: ')
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == 'exit':
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print("AI: ",result.content)
+
+print(chat_history)
