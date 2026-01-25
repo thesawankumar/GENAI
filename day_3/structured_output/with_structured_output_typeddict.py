dc548d3b7caae703eaa21d6064acaad8c993804c
thesawankumar <sawankushwaha222@gmail.com>
2026-01-25 11:39:22 +0530

    Structured Output Typedict pydantic json format and demo all idea of using Langchain

---
+++ day_3/structured_output/with_structured_output_typeddict.py
@@ -0,0 +1,40 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from typing import TypedDict,Annotated
+
+load_dotenv()
+
+model=ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+
+#schema
+class Review(TypedDict):
+    summary: Annotated[str, "A brief summary of the review"]
+    sentiment: Annotated[str, "Return sentiment of the review either positive, negative, or neutral"]
+    pros: Annotated[list[str], "List of pros mentioned in the review"]
+
+structured_model=model.with_structured_output(Review)
+
+
+
+# result=structured_model.invoke(
+    # "Give a summary and sentiment of the following review: 'The product was excellent and exceeded my expectations!'")
+
+
+
+result = structured_model.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+                                 
+Review by Nitish Singh
+""")
+
+print(result)
+print(type(result))
